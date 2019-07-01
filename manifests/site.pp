ee629afcf9c41f705d6124e66047112d52e21da0
phxfalcon <52261107+phxfalcon@users.noreply.github.com>
2019-07-01 10:20:51 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,14 @@
 node default {
   }
+  
 node 'puppet.master.vm' {
   include role::master_server
+  }
+  
+node /^web/ {
+  include role::app_server
+  }
+  
+node /^db/ {
+  include role::db_server
   }
